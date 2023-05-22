51a77c1218e743fd739a86f2af96151313d96bef
ArcherUz <berialyanarcheruz@outlook.com>
2023-05-22 17:33:43 -0600

    new

--- carts/views.py
+++ carts/views.py
@@ -64,7 +64,7 @@ def add_cart(request, product_id):
             item = CartItem.objects.create(product=product, quantity=1, cart=cart)
             if len(product_variation) > 0:
                 item.variations.clear()
-                item.variations.add(*product_variation)
+                item.variations.add(*product_variation)# * for unpack the list
             #cart_item.quantity += 1
             item.save()
     else:
