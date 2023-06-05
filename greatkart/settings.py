3f09dd443b8ba29ea349d1df95abee4165c96dd2
ArcherUz <berialyanarcheruz@outlook.com>
2023-06-04 22:55:22 -0600

    Account activation and forgotpassword

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -146,3 +146,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS={
     messages.ERROR: 'danger',
 }
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'pcyraventac@gmail.com'
+EMAIL_HOST_PASSWORD = 'olmnsaidltpcwlfe'
+EMAIL_USE_TLS = True
