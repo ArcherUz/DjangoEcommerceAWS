f8b5831cf98772fbc4822554a9bf704d825fd351
ArcherUz <berialyanarcheruz@outlook.com>
2023-06-11 16:08:19 -0600

    cart checkout

--- carts/views.py
+++ carts/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product, Variation
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -106,6 +106,34 @@ def remove_cart_item(request, product_id, cart_item_id):
 def cart(request, total=0, quantity=0,cart_item=None):
     try:
         tax = 0
+        grand_total = 0
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (5* total)/100
+        grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass
+
+    context = {
+        'total' : total,
+        'quantity': quantity,
+        'cart_items' : cart_items,
+        'tax':tax,
+        'grand_total':grand_total,
+    }
+    return render(request, 'store/cart.html',context)
+
+
+@login_required(login_url='login')
+def checkout(request,total=0, quantity=0,cart_item=None):
+    try:
+        tax = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
@@ -123,4 +151,4 @@ def cart(request, total=0, quantity=0,cart_item=None):
         'tax':tax,
         'grand_total':grand_total,
     }
-    return render(request, 'store/cart.html',context)
+    return render(request, 'store/checkout.html',context)
